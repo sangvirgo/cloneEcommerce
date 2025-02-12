38b25e4c62210b7aa39487964850f817813ba39a
Sang Virgo <tansang06092004@gmail.com>
2025-02-12 23:54:21 +0700

    mung 15

---
+++ font/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import HomePage from './components/HomeCarosel/HomePage'
+import Navigation from './components/Navigation'
+
+function App() {
+  return (
+    <div className="">
+      <Navigation />
+
+    <div>
+      <HomePage />
+    </div>
+    </div>
+
+  )
+}
+
+export default App
---
+++ font/src/components/HomePage.jsx
@@ -0,0 +1,16 @@
+import MainCarousel from "./HomeCarousel/MainCarousel";
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousel />
+
+
+        <div>
+            other sections
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
